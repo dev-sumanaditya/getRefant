3b2d6ad95ada7d736c340fa6e7cd3e17245c1d46
ad4frndz <ad4frndz@gmail.com>
2020-04-14 20:18:02 +0530

    added home cover

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PanelComponent } from './panel/panel.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PanelComponent, children: [
+    {path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
