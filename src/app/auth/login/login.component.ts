4387fcd238033ab0443facf1bda0fa74a546257b
ad4frndz <ad4frndz@gmail.com>
2020-04-15 23:50:51 +0530

    added login section

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public emailLogin = false;
+  public verifying = false;
+
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
+  public loginWithEmail() {
+    this.emailLogin = true;
+  }
+
+  public login() {
+    this.verifying = true;
+  }
 
 }
