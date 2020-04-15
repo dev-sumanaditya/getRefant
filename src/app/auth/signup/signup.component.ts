d2a4652daec9f72d775b0afd3d095f8108eeae63
ad4frndz <ad4frndz@gmail.com>
2020-04-16 05:01:41 +0530

    added signup page

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  public verifying = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public login() {
+    this.verifying = true;
+  }
 }
